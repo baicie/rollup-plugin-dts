7a67d1c7ef40c510084a2b40fb4989bbf425ff38
baicie <2055572879@qq.com>
2026-05-27 10:46:38 +0800

    release: @baicie/plugin-dts@0.0.1

---
+++ scripts/publish.ts
@@ -0,0 +1,3 @@
+import { publish } from '@baicie/release'
+
+publish({ defaultPackage: 'rollup-plugin-dts' })
---
+++ scripts/release.ts
@@ -0,0 +1,9 @@
+import { release, generateChangelog } from "@baicie/release";
+
+release({
+  repo: "baicie/rollup-plugin-dts",
+  packages: ["@baicie/plugin-dts"],
+  toTag: (pkg, version) => `${pkg}@${version}`,
+  generateChangelog: (_pkg) => {},
+  getPkgDir: () => ".", // 指定根目录
+});
